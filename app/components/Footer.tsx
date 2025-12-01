ca9c6fbfc8cbb87e3ccc7a2898b1ef3087facb23
Mitchel Okoh <mitchelokoh@gmail.com>
2025-12-01 15:10:02 +0100

    linked socialm acounts

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -36,8 +36,8 @@ export default function Footer() {
                             </h3>
 
                             {/* Learn More Button */}
-                            <a 
-                               href="https://ibx2026.eventcrib.com"
+                            <a
+                                href="https://ibx2026.eventcrib.com"
                                 className="border border-white text-white px-6 py-2 rounded-lg text-sm font-medium hover:bg-white hover:text-black transition inline-block"
                             >
                                 Register Now
@@ -79,23 +79,13 @@ export default function Footer() {
                             rel="noopener noreferrer">
                             <Twitter className="w-5 h-5" />
                         </a>
-                        <a href="/maintenance" className="w-10 h-10 flex items-center justify-center hover:text-orange-500 transition" aria-label="LinkedIn" target="_blank"
+                        <a href="https://www.linkedin.com/company/ibom-blockchain-summit/" className="w-10 h-10 flex items-center justify-center hover:text-orange-500 transition" aria-label="LinkedIn" target="_blank"
                             rel="noopener noreferrer">
                             <Linkedin className="w-5 h-5" />
                         </a>
                         <a href="https://www.facebook.com/share/16rxXKqb8m/" className="w-10 h-10 flex items-center justify-center hover:text-orange-500 transition" aria-label="Facebook" target="_blank"
                             rel="noopener noreferrer">
                             <Facebook className="w-5 h-5" />
-                        </a>
-                        <a href="/maintenance" className="w-10 h-10 flex items-center justify-center hover:text-orange-500 transition" aria-label="TikTok" target="_blank"
-                            rel="noopener noreferrer">
-                            <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M19.59 6.69a4.83 4.83 0 01-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 01-5.2 1.74 2.89 2.89 0 012.31-4.64 2.93 2.93 0 01.88.13V9.4a6.84 6.84 0 00-1-.05A6.33 6.33 0 005 20.1a6.34 6.34 0 0010.86-4.43v-7a8.16 8.16 0 004.77 1.52v-3.4a4.85 4.85 0 01-1.04-.1z" />
-                            </svg>
-                        </a>
-                        <a href="/maintenance" className="w-10 h-10 flex items-center justify-center hover:text-orange-500 transition" aria-label="Instagram" target="_blank"
-                            rel="noopener noreferrer">
-                            <Instagram className="w-5 h-5" />
                         </a>
                     </div>
                 </div>
